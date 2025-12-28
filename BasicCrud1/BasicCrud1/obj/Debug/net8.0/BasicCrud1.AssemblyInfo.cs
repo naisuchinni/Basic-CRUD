9ca1ee0ae4fab6d8e780361ac8e93b7cf34e8d73
sivalakshmanna07 <sivalakshmanna07@gmail.com>
2025-12-28 14:34:05 +0530

    Intial commit

--- BasicCrud1/BasicCrud1/obj/Debug/net8.0/BasicCrud1.AssemblyInfo.cs
+++ BasicCrud1/BasicCrud1/obj/Debug/net8.0/BasicCrud1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicCrud1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67fbbdbe6b20ed55fdc562af6dc01d8b9e55d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6138eee167d5e883dac19b8d85f654b648df5465")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicCrud1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicCrud1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
